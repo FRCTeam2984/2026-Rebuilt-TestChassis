dec3a5f8a076761384cd6371e516bd25c0c4c206
Programming Desktop 2 <ljhsvikingrobotics@gmail.com>
2026-02-13 10:25:45 -0600

    LEDs no longer complain, can be either port

--- src/main/java/frc/robot/subsystems/LED.java
+++ src/main/java/frc/robot/subsystems/LED.java
@@ -2,8 +2,21 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.SerialPort;
 
 public class LED {
-    public static SerialPort arduino = new SerialPort(9600, SerialPort.Port.kUSB1);
+    public static SerialPort arduino = null;
+    public static Boolean working = false;
 
+    public static void assignPort(){
+        try{
+            arduino = new SerialPort(9600, SerialPort.Port.kUSB1);
+            working = true;
+        }catch(Exception e){
+            try{
+                arduino = new SerialPort(9600, SerialPort.Port.kUSB);
+                working = true;
+            }catch(Exception e1){
+            }
+        }
+    }
     public static char curPattern = 'w';
 
     public static void setPattern(char newPattern){
@@ -16,7 +29,11 @@ public class LED {
             }
             curPattern = newPattern;
             System.out.println(newPattern);
-            arduino.writeString(""+newPattern);
+            try{
+                arduino.writeString(""+newPattern);
+            }catch(Exception e){
+                System.out.println("UH OH leds failed");
+            }
         }
     }
 
